a5eff014d4e233475e804cec977da70077b289c3
mikwieleb <mik.wieleb@gmail.com>
2025-04-23 14:34:43 +0200

    Update App.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import React, { useEffect } from 'react';
-import Pi from './pi-sdk'; // vérifie que ce chemin est correct
+import React from 'react';
+import PiPaymentButton from './PiPaymentButton';
 
 function App() {
-  useEffect(() => {
-    const authenticateUser = async () => {
-      try {
-        const scopes = ['payments'];
-        const auth = await Pi.authenticate(scopes, (payment) => {
-          console.log('Paiement incomplet trouvé :', payment);
-        });
-        console.log('Utilisateur Pi authentifié :', auth);
-      } catch (error) {
-        console.error('Erreur pendant l’authentification Pi :', error);
-      }
-    };
-
-    if (Pi) {
-      authenticateUser();
-    } else {
-      console.warn('SDK Pi non trouvé.');
-    }
-  }, []);
-
   return (
-    <div>
-      <h1>Test paiement Pi</h1>
-      {/* Ton bouton de paiement ici */}
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Test Paiement Pi</h1>
+      <PiPaymentButton />
     </div>
   );
 }
