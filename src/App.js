de4cf3d820096641f34008b4d3922fd710d54cb7
mikwieleb <mik.wieleb@gmail.com>
2025-04-23 11:42:19 +0200

    Update App.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-// src/App.js
 import React, { useEffect } from 'react';
-import { loadPiSDK } from './utils/pi-sdk';
-import PiPaymentButton from './PiPaymentButton';
+import Pi from './pi-sdk'; // vérifie que ce chemin est correct
 
 function App() {
   useEffect(() => {
-    // Charger le SDK Pi à l'initialisation du composant
-    loadPiSDK();
+    const authenticateUser = async () => {
+      try {
+        const scopes = ['payments'];
+        const auth = await Pi.authenticate(scopes, (payment) => {
+          console.log('Paiement incomplet trouvé :', payment);
+        });
+        console.log('Utilisateur Pi authentifié :', auth);
+      } catch (error) {
+        console.error('Erreur pendant l’authentification Pi :', error);
+      }
+    };
+
+    if (Pi) {
+      authenticateUser();
+    } else {
+      console.warn('SDK Pi non trouvé.');
+    }
   }, []);
 
   return (
-    <div className="App">
-      <h1>Test Paiement Pi</h1>
-      <PiPaymentButton />
+    <div>
+      <h1>Test paiement Pi</h1>
+      {/* Ton bouton de paiement ici */}
     </div>
   );
 }
