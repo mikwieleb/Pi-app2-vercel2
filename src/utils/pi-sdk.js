709d9404fe541977f15c4c78509ca5b69de18374
mikwieleb <mik.wieleb@gmail.com>
2025-04-25 00:02:26 +0200

    Update pi-sdk.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

--- src/utils/pi-sdk.js
+++ src/utils/pi-sdk.js
@@ -1,16 +1,5 @@
 // src/pi-sdk.js
-
-export function initPiNetwork(retries = 10, delay = 300) {
-  if (window.Pi) {
-    window.Pi.init({
-      version: "2.0",
-      sandbox: true,
-    });
-    console.log("Pi SDK initialisé avec succès.");
-  } else if (retries > 0) {
-    console.warn("Pi SDK pas encore prêt, nouvelle tentative...");
-    setTimeout(() => initPiNetwork(retries - 1, delay), delay);
-  } else {
-    console.error("Erreur : Pi SDK non chargé après plusieurs tentatives.");
-  }
+export function initPiNetwork() {
+  // NE RIEN FAIRE ICI
+  // Le Pi.init() est déjà appelé dans index.html
 }
