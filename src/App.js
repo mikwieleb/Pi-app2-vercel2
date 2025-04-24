4e6d9b59450343e3c3b28833c15e9262f88cf893
mikwieleb <mik.wieleb@gmail.com>
2025-04-25 00:17:03 +0200

    Update App.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-// src/App.js
-import React, { useEffect } from "react";
-import { initPiNetwork } from "./pi-sdk";
+import React, { useEffect, useState } from "react";
+import { authenticateWithPi } from "./pi-sdk";
+import PiPaymentButton from "./PiPaymentButton";
+import logo from "./logo.png";
 
 function App() {
+  const [user, setUser] = useState(null);
+
   useEffect(() => {
-    initPiNetwork();
+    authenticateWithPi()
+      .then((userData) => {
+        setUser(userData);
+      })
+      .catch((err) => {
+        console.error("Erreur d'authentification Pi :", err);
+      });
   }, []);
 
   return (
-    <div>
-      {/* ton UI ici */}
+    <div className="App">
+      <img src={logo} alt="Logo" className="logo" />
+      <h1>Bienvenue sur l'application Pi Network</h1>
+
+      {!user ? (
+        <p>Connexion en cours...</p>
+      ) : (
+        <>
+          <p>Connecté en tant que : {user.username}</p>
+          <PiPaymentButton />
+        </>
+      )}
     </div>
   );
 }
