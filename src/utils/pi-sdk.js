29d64ec7f8e7096c813590341f4b543d9e1f65e1
mikwieleb <mik.wieleb@gmail.com>
2025-04-25 00:32:09 +0200

    Update pi-sdk.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

--- src/utils/pi-sdk.js
+++ src/utils/pi-sdk.js
@@ -1,33 +1,22 @@
-function waitForPiSDK() {
-  return new Promise((resolve, reject) => {
-    const checkInterval = setInterval(() => {
-      if (window.Pi && window.Pi.authenticate) {
-        clearInterval(checkInterval);
-        resolve();
-      }
-    }, 100);
-    setTimeout(() => {
-      clearInterval(checkInterval);
-      reject("Pi SDK non chargé à temps");
-    }, 5000); // max 5 secondes
-  });
-}
-
 export async function authenticateWithPi() {
-  try {
-    await waitForPiSDK();
-
-    const scopes = ["username", "wallet"];
-    return new Promise((resolve, reject) => {
-      window.Pi.authenticate(scopes, (authData) => {
-        if (authData && authData.username) {
-          resolve(authData);
-        } else {
-          reject("Authentification échouée");
-        }
-      });
-    });
-  } catch (err) {
-    throw new Error("Erreur SDK : " + err);
+  if (!window.Pi) {
+    throw new Error("Pi SDK non chargé");
   }
+
+  try {
+    window.Pi.init({ version: "2.0", sandbox: true }); // Initialisation ici
+  } catch (e) {
+    console.warn("Pi.init déjà appelé ou erreur : ", e);
+  }
+
+  return new Promise((resolve, reject) => {
+    const scopes = ["username", "wallet"];
+    window.Pi.authenticate(scopes, (authData) => {
+      if (authData && authData.username) {
+        resolve(authData);
+      } else {
+        reject("Authentification échouée");
+      }
+    });
+  });
 }
