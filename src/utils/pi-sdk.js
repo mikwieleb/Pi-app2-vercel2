0ee52e9aa5231fa69619429c4188d69d016f543c
mikwieleb <mik.wieleb@gmail.com>
2025-04-25 00:47:31 +0200

    Update pi-sdk.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

--- src/utils/pi-sdk.js
+++ src/utils/pi-sdk.js
@@ -1,13 +1,27 @@
-export async function authenticateWithPi() {
-  if (!window.Pi) {
-    throw new Error("Pi SDK non chargé");
-  }
+function waitForPiInit() {
+  return new Promise((resolve, reject) => {
+    const start = Date.now();
+    const interval = setInterval(() => {
+      if (window.Pi && window.Pi.authenticate) {
+        try {
+          window.Pi.init({ version: "2.0", sandbox: true });
+          clearInterval(interval);
+          resolve();
+        } catch (e) {
+          clearInterval(interval);
+          reject("Erreur pendant Pi.init : " + e);
+        }
+      }
+      if (Date.now() - start > 5000) {
+        clearInterval(interval);
+        reject("Pi SDK non initialisé dans les temps.");
+      }
+    }, 100);
+  });
+}
 
-  try {
-    window.Pi.init({ version: "2.0", sandbox: true }); // Initialisation ici
-  } catch (e) {
-    console.warn("Pi.init déjà appelé ou erreur : ", e);
-  }
+export async function authenticateWithPi() {
+  await waitForPiInit();
 
   return new Promise((resolve, reject) => {
     const scopes = ["username", "wallet"];
