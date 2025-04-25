52a9c2f0b720d420baa426cca04395af4a805606
mikwieleb <mik.wieleb@gmail.com>
2025-04-25 02:28:53 +0200

    Update App.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,34 +1,3 @@
-import React, { useEffect, useState } from "react";
-import { authenticateWithPi } from './utils/pi-sdk';
-
-function App() {
-  const [authStatus, setAuthStatus] = useState(null);
-
-  useEffect(() => {
-    const authenticate = async () => {
-      try {
-        const auth = await authenticateWithPi();
-        setAuthStatus(auth.user.username);
-      } catch (error) {
-        setAuthStatus("Erreur d'authentification");
-      }
-    };
-    
-    authenticate();
-  }, []);
-
-  return (
-    <div className="App">
-      <h1>Vente Automobile Pi</h1>
-      {authStatus ? (
-        <p>Bienvenue, {authStatus}</p>
-      ) : (
-        <p>Authentification en cours...</p>
-      )}
-      <button onClick={() => alert("Ouvrir l'application")}>Ouvrir l'application</button>
-      <button onClick={() => alert("Payer 0,001 Pi")}>Payer 0.001 Pi</button>
-    </div>
-  );
-}
-
-export default App;
+useEffect(() => {
+  initPiNetwork();
+}, []);
