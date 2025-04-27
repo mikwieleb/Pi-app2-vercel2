fb8b093a8c01156cf7cba68847a7e01275ba59cc
mikwieleb <mik.wieleb@gmail.com>
2025-04-27 19:40:03 +0200

    Update pi-sdk.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

--- src/utils/pi-sdk.js
+++ src/utils/pi-sdk.js
@@ -1,16 +1,22 @@
-// src/utils/pi-sdk.js
+// src/pi-sdk.js
 
-export const initiatePayment = async (username) => {
-  try {
-    const paymentData = {
-      amount: 0.001,
-      memo: "Paiement Vente Automobile",
-      metadata: { username },
-    };
-
-    const payment = await window.Pi.createPayment(paymentData);
-    console.log('Payment created:', payment);
-  } catch (error) {
-    console.error('Payment initiation failed:', error);
+// Simulation d'un SDK Pi simplifié
+class PiSdk {
+  constructor({ appId, apiKey }) {
+    this.appId = appId;
+    this.apiKey = apiKey;
   }
-};
+
+  async payment(amount) {
+    console.log(`Paiement simulé de ${amount} Pi avec l'appId ${this.appId}`);
+    // Ici tu pourrais intégrer la vraie logique de paiement avec Pi Network SDK
+    alert(`Paiement de ${amount} Pi effectué !`);
+  }
+}
+
+const piSdk = new PiSdk({
+  appId: 'venteautomobile.pi', // Ton App ID
+  apiKey: 'tpk9grfy1kvj0vlwep4wbqtev5cumfaf4vrcoop5plkanviumkeee67w9g1nixuy', // Ta clé API Testnet
+});
+
+export default piSdk;
