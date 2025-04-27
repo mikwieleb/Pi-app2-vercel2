2120991924c1316f47c785878886f622c96be8df
mikwieleb <mik.wieleb@gmail.com>
2025-04-27 22:35:50 +0200

    Update App.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 // src/App.js
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PiPaymentButton from './components/PiPaymentButton';
 import { initializePiSdk } from './utils/pi-sdk'; // On importe l'initialisation du SDK
 import './App.css';
 
 function App() {
-  React.useEffect(() => {
-    initializePiSdk();  // Assure-toi que le SDK Pi est initialisé dès le chargement
+  const [sdkInitialized, setSdkInitialized] = useState(false);
+
+  useEffect(() => {
+    // Initialiser le SDK Pi dès que l'application se charge
+    initializePiSdk();
+    setSdkInitialized(true);
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Vente Automobile Pi</h1>
-        
-        <PiPaymentButton />
+
+        {sdkInitialized ? (
+          <PiPaymentButton /> // Afficher le bouton une fois que le SDK est initialisé
+        ) : (
+          <p>Chargement du SDK Pi...</p>
+        )}
       </header>
     </div>
   );
