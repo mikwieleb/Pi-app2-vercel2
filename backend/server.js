2e29e00e163e0a552e5b32929fc2208d8dc171a1
mikwieleb <mik.wieleb@gmail.com>
2025-04-29 00:54:58 +0200

    Create server.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
