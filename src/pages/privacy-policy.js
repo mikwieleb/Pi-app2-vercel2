8df74afccd694aab14a49ba85836c80646a8f42f
mikwieleb <mik.wieleb@gmail.com>
2025-05-05 23:48:33 +0200

    Create privacy-policy.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PrivacyPolicy() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Politique de confidentialité</h1>
+      <p>Nous respectons votre vie privée. Toutes les données sont traitées avec confidentialité...</p>
+    </div>
+  );
+}
