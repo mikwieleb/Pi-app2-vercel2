17a9f725aa856afc2867cbc13ed39d8481f7073b
mikwieleb <mik.wieleb@gmail.com>
2025-05-05 23:49:21 +0200

    Create terms-of-service.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

---
+++ src/pages/terms-of-service.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TermsOfService() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Conditions générales d'utilisation</h1>
+      <p>En utilisant cette application, vous acceptez les conditions suivantes...</p>
+    </div>
+  );
+}
