8b0885453516d5e93d78a78cf5bb51a2ad27fcb3
mikwieleb <mik.wieleb@gmail.com>
2025-05-06 08:00:49 +0200

    Update App.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import './App.css';
 import PiPaymentButton from './PiPaymentButton';
 
 function App() {
+  const handleOpenApp = () => {
+    alert("L'application est déjà ouverte dans le Pi Browser.");
+  };
+
   return (
-    <div style={{ textAlign: 'center', marginTop: 50 }}>
-      <h1 style={{ color: '#6A0DAD' }}>Vente Automobile en Pi</h1>
-      <img src="/logo.png" alt="Logo" style={{ width: 150 }} />
-      <br /><br />
-      <a href="https://vente-automobile-pi-g6hd.vercel.app" target="_blank" rel="noopener noreferrer">
-        <button>Ouvrir l'application</button>
-      </a>
-      <br /><br />
-      <PiPaymentButton />
+    <div className="App">
+      <header className="App-header">
+        <img src="/logo.png" alt="Logo" className="App-logo" />
+        <h1>Bienvenue dans l'application Pi</h1>
+
+        <PiPaymentButton />
+
+        <button className="open-btn" onClick={handleOpenApp}>
+          Ouvrir l'application
+        </button>
+      </header>
     </div>
   );
 }
