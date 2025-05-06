51b764e564a7746668008bbc788476b9dc6529d4
mikwieleb <mik.wieleb@gmail.com>
2025-05-06 07:39:00 +0200

    Create terms-of-service.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

---
+++ src/pages/terms-of-service.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function TermsOfService() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Conditions Générales d’Utilisation</h1>
+      <p>
+        En utilisant cette application, vous acceptez de respecter les règles du réseau Pi. 
+        Cette application est fournie telle quelle, sans garantie. Nous ne sommes pas responsables 
+        des pertes ou erreurs liées à son utilisation.
+      </p>
+    </div>
+  );
+}
+
+export default TermsOfService;
