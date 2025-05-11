342d397c9860336d9bf662e02fb4926780ae0570
mikwieleb <mik.wieleb@gmail.com>
2025-05-11 22:08:02 +0200

    Update pi-sdk.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

--- src/pi-sdk.js
+++ src/pi-sdk.js
@@ -1,11 +1,10 @@
-// pi-sdk.js
-export const Pi = window.Pi;
+// src/pi-sdk.js
+import { Pi } from '@pinetwork-js/sdk';
 
-if (typeof Pi !== 'undefined') {
-  Pi.init({
-    version: "2.0",
-    sandbox: true, // Important : testnet Pi
-  });
-} else {
-  console.warn("Le SDK Pi n'est pas disponible. Assurez-vous d'ouvrir l'application dans Pi Browser.");
-}
+const PiSDK = Pi.init({
+  version: '2.0',
+  sandbox: true,
+  appId: 'venteautomobile.pi',
+});
+
+export default PiSDK;
