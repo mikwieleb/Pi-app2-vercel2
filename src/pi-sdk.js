1adbade7a2808b982fe45243d0e096afe020870d
mikwieleb <mik.wieleb@gmail.com>
2025-05-11 12:43:54 +0200

    Update pi-sdk.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

--- src/pi-sdk.js
+++ src/pi-sdk.js
@@ -1,8 +1,11 @@
+// pi-sdk.js
 export const Pi = window.Pi;
 
-if (Pi) {
+if (typeof Pi !== 'undefined') {
   Pi.init({
     version: "2.0",
-    sandbox: true,
+    sandbox: true, // Important : testnet Pi
   });
+} else {
+  console.warn("Le SDK Pi n'est pas disponible. Assurez-vous d'ouvrir l'application dans Pi Browser.");
 }
