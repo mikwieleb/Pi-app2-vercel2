85ea717a17be367585911f63af39102b604dd679
mikwieleb <mik.wieleb@gmail.com>
2025-05-31 01:13:33 +0200

    Create App.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Pi from "./pi-sdk";
+import PiPaymentButton from "./PiPaymentButton";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px", fontFamily: "Arial" }}>
+      <h1>Vente Automobile 🚗</h1>
+      <button
+        onClick={() => window.open("https://vente-automobile-pi-g6hd.vercel.app", "_blank")}
+        style={{ padding: "10px 20px", marginBottom: "20px", backgroundColor: "#a64ca6", color: "white", border: "none", borderRadius: "5px" }}
+      >
+        Ouvrir l'application
+      </button>
+      <PiPaymentButton />
+    </div>
+  );
+}
+
+export default App;
