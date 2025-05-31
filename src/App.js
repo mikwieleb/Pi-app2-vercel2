e7bb16e7a7f5b828295e94d41fafee758e8caa96
mikwieleb <mik.wieleb@gmail.com>
2025-05-31 02:21:39 +0200

    Update App.js
    Signed-off-by: mikwieleb <mik.wieleb@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from "react";
 import Pi from "./pi-sdk";
 import PiPaymentButton from "./PiPaymentButton";
 
-console.log("✅ App.js est chargé");
-
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "100px", fontFamily: "Arial" }}>
-      <h1>✅ Vente Automobile est en ligne</h1>
+      <h1>Vente Automobile 🚗</h1>
       <button
         onClick={() => window.open("https://vente-automobile-pi-g6hd.vercel.app", "_blank")}
         style={{ padding: "10px 20px", marginBottom: "20px", backgroundColor: "#a64ca6", color: "white", border: "none", borderRadius: "5px" }}
